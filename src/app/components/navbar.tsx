13c0501904c8a4aaa5ed4fb5daf8e0c975d642ee
RigersUliu <83981306+RigersUliu@users.noreply.github.com>
2025-03-28 09:09:31 +0100

    small changes on the navbar for mobile, removed two dead links

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -37,6 +37,7 @@ const Navbar = () => {
       {/* Sliding Panel for Mobile */}
       <div className={`fixed inset-0 bg-black bg-opacity-50 z-40 transition-opacity duration-300 ${isOpen ? 'opacity-100' : 'opacity-0 pointer-events-none'}`}>
         <div className={`flex flex-col items-center justify-center h-full bg-gray-900 transition-transform duration-300 transform ${isOpen ? 'translate-y-0' : '-translate-y-full'}`}>
+          <h1 className="text-white text-2xl font-bold">AI-Tour-Logo</h1>
           {/* Close Button Inside the Panel */}
           <button
             className="text-white mb-4 self-end mr-4"
@@ -56,8 +57,6 @@ const Navbar = () => {
           <ul className="space-y-4">
             <li><Link href="/" className="text-white hover:text-gray-400" onClick={toggleMenu}>Home</Link></li>
             <li><Link href="/map" className="text-white hover:text-gray-400" onClick={toggleMenu}>Destinations</Link></li>
-            <li><Link href="/contact" className="text-white hover:text-gray-400" onClick={toggleMenu}>Contact</Link></li>
-            <li><Link href="/signup" className="text-white hover:text-gray-400" onClick={toggleMenu}>Sign-up</Link></li>
           </ul>
         </div>
       </div>
